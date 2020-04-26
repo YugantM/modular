abb7a101f479b1deaa6b36d6883869aab9cbebb1
Yugant <hadiyalyugant@gmail.com>
2020-04-26 14:52:30 +0200

    updates

--- testing.py
+++ testing.py
@@ -1,15 +1,10 @@
-import requests,json,urllib
-
-url = 'http://127.0.0.1:5000/sql_generate'
+import sys
 
 
-with open('main.json') as fh:
-    da = json.load(fh)
+def main():
+	print(sys.argv[0])
 
-da = json.dumps(da)
-x = requests.post(url, params = da)
 
-#print(da,'\n\n')
 
-print(x.status_code)
-
+if __name__ == "__main__":
+    main()
